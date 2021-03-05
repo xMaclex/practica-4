bff7ac042baedecf9729ce57a05f3e5291bd8f59
xMaclex <62273275+xMaclex@users.noreply.github.com>
2021-03-04 22:23:32 -0400

    Add files via upload

---
+++ practica4-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace practica4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numero = {0,0,0,0,0};
+            Console.WriteLine("Introduzca 5 numeros: ");
+            numero[0] = int.Parse(Console.ReadLine());
+            numero[1] = int.Parse(Console.ReadLine());
+            numero[2] = int.Parse(Console.ReadLine());
+            numero[3] = int.Parse(Console.ReadLine());
+            numero[4] = int.Parse(Console.ReadLine());
+            Console.WriteLine("RESULTADO");
+
+            Array.Reverse(numero);
+            for (int i = 0; i <=4;i++)
+            {
+                Console.WriteLine(numero[i]);
+            }
+        }
+    }
+}
